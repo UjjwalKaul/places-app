f2ce4c87534ed2409da82cc3671840f9ae579d89
Ujjawal Kaul <ujjwal171@outlook.com>
2024-05-16 21:16:50 +0530

    Edit fav place items

--- components/PlacesList.js
+++ components/PlacesList.js
@@ -1,13 +1,37 @@
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 import React from 'react';
+import PlaceItem from './PlaceItem';
 
 export default function PlacesList({ places }) {
+  if (!places || places.length === 0) {
+    return (
+      <View style={styles.fallback}>
+        <Text style={styles.fallbackText}>
+          No places added yet - Start adding some
+        </Text>
+      </View>
+    );
+  }
   return (
     <FlatList
       data={places}
       keyExtractor={(item) => {
         return item.id;
       }}
+      renderItem={({ item }) => {
+        return <PlaceItem place={item} />;
+      }}
     />
   );
 }
+
+const styles = StyleSheet.create({
+  fallback: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  fallbackText: {
+    fontSize: 16,
+  },
+});
