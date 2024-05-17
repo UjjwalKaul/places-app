0f6ad3d50f81011947efef71cadf3136810710b6
Ujjawal Kaul <ujjwal171@outlook.com>
2024-05-17 21:08:08 +0530

    Added locating user

--- components/LocationPicker.js
+++ components/LocationPicker.js
@@ -1,10 +1,38 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Alert, StyleSheet } from 'react-native';
 import React from 'react';
 import OutlinedButton from './ui/OutlinedButton';
 import { Colors } from '../colors';
+import {
+  getCurrentPositionAsync,
+  useForegroundPermissions,
+  PermissionStatus,
+} from 'expo-location';
 
 export default function LocationPicker() {
-  function getLocationHandler() {}
+  const [locationPermissionInfo, requestPermission] =
+    useForegroundPermissions();
+  async function verifyPermissions() {
+    if (locationPermissionInfo.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+    if (locationPermissionInfo.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        'Permission Denied',
+        'You need to grant location permissions to use this app'
+      );
+      return false;
+    }
+    return true;
+  }
+  async function getLocationHandler() {
+    const hasPermissions = await verifyPermissions();
+    if (!hasPermissions) {
+      return;
+    }
+    const location = await getCurrentPositionAsync();
+    console.log(location);
+  }
   function pickOnMapHandler() {}
   return (
     <View>
